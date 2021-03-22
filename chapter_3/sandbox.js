92bb05c73d995d3fff82a384b9dbe073d08a7045
Colin Harnish <Harnish.colin@gmail.com>
2021-03-21 19:22:28 -0600

    save point

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,73 +1,24 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+console.log("hello friend")
 
-ninjas[1] = 'ken';
+// for loops
 
-console.log(ninjas)
+for(let i = 0; i < 5; i++){
+    console.log('in loop:', i);
+}
 
-let ages = [20,25,30,35];
+console.log('loop finish')
 
-console.log(ages)
+const names = ['shaun', 'mario', 'luigi'];
 
-let random = ['shaun', 'crystal', 30, 20];
+for(let b = 0; b < names.length; b++){
+    console.log(names[b]);
+    let html = `<div>${names[b]}</div>`
+    console.log(html)
+}
 
-console.log(random);
+let c = 0;
 
-console.log(ninjas.length);
-
-let result = ninjas.join(' and ');
-
-console.log(result);
-
-let result2 = ninjas.indexOf('chun-li');
-
-console.log(result2);
-
-let result3 = ninjas.concat('ken', 'crystal');
-
-console.log(result3);
-
-let result4 = ninjas.push('ken');
-
-console.log(result4)
-
-let result5 = ninjas.pop()
-
-console.log(result5)
-console.log(result4)
-
-
-//NULL
-
-let age = null;
-
-console.log(age, age + 3, `the age is ${age}`)
-
-//booleans
-console.log(true, false, 'true', 'false');
-
-let email = 'luigi@thenetninja.co.uk';
-let names = ['mario', 'luigi', 'tooad'];
-
-let result6 = email.includes('@')
-
-console.log(result6)
-
-let result7 = names.includes('luigi')
-console.log(result7)
-
-let age2 = 25;
-
-console.log(age2 == 25);
-console.log(age2 == 30);
-console.log(age2 != 30); // != means is "not equal"
-console.log(age2 > 20);
-console.log(age2 < 20);
-console.log(age2 <= 25);
-console.log(age2 >= 25);
-
-let name2 = 'colin';
-
-console.log(name2 == 'colin') //loose equality doesn't consider value type
-console.log(name2 == 'Colin')
-console.log(name2 > 'amanda')
-console.log('Amanda' > 'amanda')  //java is alphabetical and looks for capital letters. 
+while(c < 1000){
+    console.log('in loop: ', c);
+    c++;
+}
