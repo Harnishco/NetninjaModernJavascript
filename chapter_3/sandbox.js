8bf0dd7e1914ad6283c447f67355be3dffbded1b
Colin Harnish <Harnish.colin@gmail.com>
2021-03-31 19:28:07 -0600

    save point

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -37,4 +37,63 @@ do{
 }
 while(e < 5)
 
-console.log('https://youtu.be/JloLGV9DmtQ?list=PL4cUxeGkcC9haFPT7J25Q9GRB_ZkFrQAc&t=1035')
+// if statements
+
+const age = 25;
+
+if(age > 20){
+    console.log('you are over 20 years old')
+}
+
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+if(ninjas.length > 3){
+    console.log("that's a lot of ninjas")
+}
+
+// logical operators - OR || and AND &&
+
+const password = 'pass@word12345678';
+
+if(password.length >= 12 && password.includes("@")){
+    console.log('that password is very strong')
+} else if(password.length >= 8 || password.includes("#","!")&& password.length > 5){
+    console.log('that password is long enough but does not contain the correct symbols')
+} else{ //else is for when the opposite of if happens
+    console.log('password is not long enough')
+} 
+
+//logical NOT (!)
+
+let user = false;
+
+if(!user){
+    console.log("Wrong Username");
+}
+
+console.log(!true)
+console.log(!false)
+
+//break and continue
+
+const scores = [50, 25, 0, 30, 100, 20, 10]
+
+for(let i = 0; scores.length; i++){
+    
+    if(scores[i] === 0){
+        continue;
+    }
+        
+
+    console.log('your score: ', scores[i])
+
+    if(scores[i] === 100){
+        console.log('congrats, you got the top score!');
+        break;
+
+    }
+    
+}
+
+
+
