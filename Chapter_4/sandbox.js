afc1c3f0d2564640cfad25843fdc721889855063
Colin Harnish <Harnish.colin@gmail.com>
2021-04-01 20:26:03 -0600

    saving for the night

--- Chapter_4/sandbox.js
+++ Chapter_4/sandbox.js
@@ -1,1 +1,96 @@
-//https://youtu.be/xUI5Tsl2JpY?list=PL4cUxeGkcC9haFPT7J25Q9GRB_ZkFrQAc&t=99
+//function declaration
+/* 
+function greet(){
+    console.log('hello there'); */
+/* } */
+
+//function expression
+
+/* const speak = function(){
+    console.log('good day')
+}; */
+
+/* greet();
+greet();
+greet();
+
+ */
+/* speak();
+speak();
+speak(); */
+
+// arguments % parameters
+
+/* const speak = function(name, time){
+    console.log(`good ${time} ${name}`);
+};
+
+speak ('mario', 'morning')
+
+const speak = function(name, time){
+    console.log(`good ${time} ${name}`);
+};
+
+speak ('mario', 'morning')
+
+ */
+
+/* const calcArea = function(radius){
+    return 3.14 * radius**2
+}
+
+
+
+console.log(area)
+
+const calcVol = function(area){
+
+} */
+
+// arrow function
+
+/* const calcArea = (radius) => {
+    return 3.14 * radius **2;
+}
+
+const area = calcArea(5)
+console.log('area is:', area)
+
+
+
+
+
+const calcgrowth = (growth, invested) => growth * invested + invested;
+
+
+const number1= calcgrowth(.05, 100) 
+
+
+console.log("Your return is;" number1 ); */
+
+
+
+
+/* const calcArea = radius =>  3.14 * radius**2;
+
+const area = calcArea(5);
+console.log('area is:', area); */
+
+/* const greet = function(){
+    return 'hello, world';
+} */
+
+
+
+
+const greet = () => 'hello,world'
+const result = greet();
+console.log(result);
+
+const bill = function(products, tax){
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
